1e85fb4d8d22e7a78f225a57bd43bd58db32aaa0
guaraninhandeva <74692937+guaraninhandeva@users.noreply.github.com>
2020-11-18 23:25:26 -0300

    Primeiro Upload - Versão de Testes
    Falta alterar descrição do Index html

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1605752292,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"tiledbackground2.png",
+		"tiledbackground.png",
+		"spritebox-sheet0.png",
+		"spritebuttonstart-sheet0.png",
+		"mat100-sheet0.png",
+		"mat10-sheet0.png",
+		"mat1-sheet0.png",
+		"loading-logo.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"app-logo.png",
+		"logo.png"
+	]
+}
